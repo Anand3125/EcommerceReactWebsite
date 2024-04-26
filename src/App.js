cd28d00b8cf7765c8cbd60a357261efd7d4eef6b
Anand <124582976+Anand3125@users.noreply.github.com>
2024-04-27 01:48:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ function App() {
           <Route path="/products" element={ <Products/>}/>
           <Route path="/categories/:name" element={<CategoryProducts/>} />
           <Route path="cart" element={<Cart />} />
+          <Route path="/about" element={ <About/>}/>
+          <Route path="/contact" element={ <Contact/>}/>
           < Route path="*" element={<div><h1>404</h1></div>} /> 
       </Routes>
       <Footer />
