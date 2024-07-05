c465ab96adb05f918bbb3e3d9cb68131cef021f7
Anand <124582976+Anand3125@users.noreply.github.com>
2024-07-06 05:25:44 +0530

    Update index.js

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -10,7 +10,7 @@ const Hero = () => {
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
                         <br className="hidden lg:inline-block"/>readymade gluten
                     </h1>
-                    <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                    <p className="mb-8 leading-relaxed">Hi myself Anand.This project is an e-commerce website developed using React, demonstrating a comprehensive set of web development skills. It encompasses a user-friendly interface, dynamic content rendering,  and integration with fakeproduct Api Freely available on web.<br>This project highlights my ability to build a robust and scalable e-commerce application using modern web technologies, demonstrating both frontend and its related skills like here i use TailBlock a tailwind library to make its product page and all. </p>
                     <div className="flex justify-center">
                         <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
                         <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
