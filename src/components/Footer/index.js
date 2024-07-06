759c5fc24f324c091d780a4af960fabe8af83ff1
Anand <124582976+Anand3125@users.noreply.github.com>
2024-07-06 05:31:20 +0530

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -87,8 +87,8 @@ const Footer = () => {
                 </div>
                 <div className="bg-gray-100">
                     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                        <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-                            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+                        <p className="text-gray-500 text-sm text-center sm:text-left">© 2024 A website created by 
+                            <a href="https://growwithak.online/" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@Anand</a>
                         </p>
                         <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
                             <a className="text-gray-500">
